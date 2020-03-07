289067a9c0faa89421ee189ebb3d58d2f4612afc
Cristian Romero <opensource1998@gmail.com>
2020-03-07 00:32:57 -0500

    Add boostrap 4 to index.html

--- app.js
+++ app.js
@@ -13,12 +13,17 @@
 	firebase.initializeApp(firebaseConfig);
 	const mainRef = firebase.database().ref();
 
-
 	var projectConfig = function ($routeProvider) {
 		$routeProvider
 			.when('/', {
 				templateUrl: "views/gallery.html"
 			})
+			.when('/task', {
+				templateUrl: "views/task.html"
+			})
+			.otherwise({
+				redirect: '/'
+			});
 	};
 	projectConfig.$inject = ["$routeProvider"];
 	angular.module("phonecatApp", ["ngRoute"]).config(projectConfig);
